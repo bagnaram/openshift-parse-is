c12f31ec60c71f8e49fbbe81249796d4d96ddf6f
Matt Bagnara <bagnaramatt@gmail.com>
2017-08-29 10:49:14 -0400

    Update parse-imagestream.rb

--- parse-imagestream.rb
+++ parse-imagestream.rb
@@ -1,47 +1,21 @@
 #!/usr/bin/ruby
 require 'rubygems'
 require 'json'
-require 'pp'
+require 'optparse'
+require_relative 'imageStreamParser'
 
-streams = ['/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/xpaas-streams/jboss-image-streams.json', 
-           '/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/xpaas-streams/fis-image-streams.json',
-           '/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/image-streams/image-streams-rhel7.json']
 
-streams.each do |stream|
-  if !File.exist?(stream) then
-    printf("File %s not found!\nThis could mean you don't have openshift-ansible installed.\n\n", stream)
-    exit
-  end
-end
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: parse-imagestream.rb [options]"
 
-printf("###### XPASS Image Streams\n")
-json = File.read(streams[0])
-obj = JSON.parse(json)
+  opts.on('-h', '--help', 'Prints this help.') { puts opts; exit }
 
-obj['items'].each do | image |
-  printf("%s:latest\n", image["spec"]["dockerImageRepository"]);
-  image['spec']['tags'].each do | tag |
-    printf("%s:%s\n",image['spec']['dockerImageRepository'],tag['name'])
-  end
-end
+end.parse!
 
-printf("###### XPASS FIS Image Streams\n")
-json = File.read(streams[1])
-obj = JSON.parse(json)
-
-obj['items'].each do | image |
-  printf("%s:latest\n", image["spec"]["dockerImageRepository"]);
-  image['spec']['tags'].each do | tag |
-    printf("%s:%s\n",image['spec']['dockerImageRepository'],tag['name'])
-  end
-end
-
-printf("###### RHEL7 Image Streams\n")
-json = File.read(streams[2])
-obj = JSON.parse(json)
-
-obj['items'].each do | image |
-  image['spec']['tags'].each do | tag |
-      printf("%s\n",tag['from']['name']) if tag['from']['kind'] == 'DockerImage'
-  end
+parser = ImageStreamParser.new
+if parser.verifyStreams() then
+  parser.dumpStreams(tty=true)
+else
+  exit
 end
