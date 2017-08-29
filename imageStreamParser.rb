3deaadece3978a44b81b30f97a498a7f2b840a62
Matt Bagnara <bagnaramatt@gmail.com>
2017-08-29 14:33:02 -0400

    Update imageStreamParser.rb

--- imageStreamParser.rb
+++ imageStreamParser.rb
@@ -8,7 +8,37 @@ class ImageStreamParser
   @@streams = ['/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/xpaas-streams/jboss-image-streams.json', 
              '/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/xpaas-streams/fis-image-streams.json',
              '/usr/share/ansible/openshift-ansible/roles/openshift_examples/files/examples/v1.4/image-streams/image-streams-rhel7.json']
+
+  @@additional_images = ['registry.access.redhat.com/openshift3/ose-haproxy-router:tag1',
+                         'registry.access.redhat.com/openshift3/ose-deployer:tag1',
+                         'registry.access.redhat.com/openshift3/ose-recycler:tag1',
+                         'registry.access.redhat.com/openshift3/ose-sti-builder:tag1',
+                         'registry.access.redhat.com/openshift3/ose-docker-builder:tag1',
+                         'registry.access.redhat.com/openshift3/ose-pod:tag1',
+                         'docker.io/openshift/hello-openshift:latest',
+                         'registry.access.redhat.com/openshift3/ose-docker-registry:tag2',
+                         'registry.access.redhat.com/openshift3/logging-deployer:tag2',
+                         'registry.access.redhat.com/openshift3/logging-elasticsearch:tag2',
+                         'registry.access.redhat.com/openshift3/logging-kibana:tag2',
+                         'registry.access.redhat.com/openshift3/logging-fluentd:tag2',
+                         'registry.access.redhat.com/openshift3/logging-curator:tag2',
+                         'registry.access.redhat.com/openshift3/logging-auth-proxy:tag2',
+                         'registry.access.redhat.com/openshift3/metrics-deployer:tag2',
+                         'registry.access.redhat.com/openshift3/metrics-hawkular-metrics:tag2',
+                         'registry.access.redhat.com/openshift3/metrics-cassandra:tag2',
+                         'registry.access.redhat.com/openshift3/metrics-heapster:tag2']
+  @@t1='3.4.1.44'
+  @@t2='3.4.1'
+
   @images = []
+
+# Grab the correct version for the integrated images. Get these by using the version of the RPM.
+  def getOadmVersion
+    out = `yum info atomic-openshift`
+    @@t1= out.scan(/Version     : (.*)\n/).last.first
+    @@t2= @@t1.scan(/([0-9]*\.[0-9]*\.[0-9]*)\..*/).last.first
+    
+  end
 
   def verifyStreams
     @@streams.each do |stream|
@@ -55,6 +85,13 @@ class ImageStreamParser
           end
       end
     end
+    printf("###### Integrated Images\n") if tty==true
+    getOadmVersion()
+    @@additional_images=@@additional_images.map{ | img | img.gsub(/tag[12]/, 'tag1' => @@t1, 'tag2' => @@t2)}
+    @@additional_images.each do |image|
+      tty==true ? puts("#{image}\n") : @images << image
+    end
+
     return @images
   end
 
